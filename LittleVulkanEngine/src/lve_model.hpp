b4658fc796df9529f3b99d6c9c387f80047a15d9
Pieberry-G <cyh2835919524@gmail.com>
2024-08-22 17:09:12 +0800

    19-Index and Staging Buffers.

--- LittleVulkanEngine/src/lve_model.hpp
+++ LittleVulkanEngine/src/lve_model.hpp
@@ -11,7 +11,6 @@ namespace lve {
 	class LveModel
 	{
 	public:
-
 		struct Vertex {
 			glm::vec3 position;
 			glm::vec3 color;
@@ -20,7 +19,13 @@ namespace lve {
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 		};
 
-		LveModel(LveDevice& device, const std::vector<Vertex>& vertices);
+		struct Builder
+		{
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		LveModel(LveDevice& device, const LveModel::Builder& builder);
 		~LveModel();
 
 		LveModel(const LveModel&) = delete;
@@ -30,13 +35,19 @@ namespace lve {
 		void draw(VkCommandBuffer commandBuffer);
 	private:
 		void createVertexBuffers(const std::vector<Vertex>& vertices);
+		void createIndexBuffers(const std::vector<uint32_t>& indices);
 
 	private:
 		LveDevice& lveDevice;
+
 		VkBuffer vertexBuffer;
 		VkDeviceMemory vertexBufferMemory;
 		uint32_t vertexCount;
 
+		bool hasIndexBuffer = false;
+		VkBuffer indexBuffer;
+		VkDeviceMemory indexBufferMemory;
+		uint32_t indexCount;
 	};
 
 }
